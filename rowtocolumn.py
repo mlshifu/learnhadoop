fe035b4f60e6e30f10cd54f718c2e2b2d643c539
mlshifu <60336732+mlshifu@users.noreply.github.com>
2023-12-18 10:52:44 +0530

    Update rowtocolumn.py

--- rowtocolumn.py
+++ rowtocolumn.py
@@ -5,20 +5,18 @@ result_data = {'hostname': ['abc1.com', 'abc2.com', 'abc3.com', 'abc4.com', 'abc
                'value': [10, 20, 30, 40, 50, 60]}
 result_df = pd.DataFrame(result_data)
 
-# Set the timestamp for the current run
-current_timestamp = pd.to_datetime('now')
-
-# Create a new DataFrame for the final output
-final_result_df = pd.DataFrame(columns=["Timestamp"] + result_df["hostname"].tolist())
-
-# Fill in the values for the final DataFrame
-final_result_df.loc[0, "Timestamp"] = current_timestamp
-for hostname in result_df["hostname"]:
-    value = result_df[result_df["hostname"] == hostname]["value"].tolist()
-    final_result_df[hostname] = value
-
 # Set the full path for the result file
 result_file = '/home/ravi/scriptoutput/result_history.csv'
 
-# Save the final result DataFrame to the CSV file
-final_result_df.to_csv(result_file, index=False)
+# Read the existing CSV file and keep only the last 47 entries (including the current run)
+result_df_history = pd.read_csv(result_file)
+result_df_history = result_df_history.tail(47)
+
+# Set the timestamp for the current run
+current_timestamp = pd.to_datetime('now')
+
+# Add the current run to the history DataFrame
+result_df_history = result_df_history.append({"Timestamp": current_timestamp, **result_df.set_index('hostname').to_dict()['value']}, ignore_index=True)
+
+# Save the history DataFrame back to the CSV file
+result_df_history.to_csv(result_file, index=False)
