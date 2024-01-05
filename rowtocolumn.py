2af6aad4570d53299cfa0583d5779ab3a7aff057
mlshifu <60336732+mlshifu@users.noreply.github.com>
2024-01-05 23:12:46 +0530

    Update rowtocolumn.py

--- rowtocolumn.py
+++ rowtocolumn.py
@@ -8,3 +8,5 @@ hadoop fs -ls -R /path/to/parent/directory | awk '{print $3,$6,$7,$NF}' | grep -
     hadoop fs -du -s $dir | awk -v user=$user '{sum += $1} END {print user, sum/2^30 "GB"}'
   fi
 done
+
+hdfs dfs -du -h /tmp | awk '{ size=$1; unit=substr($1, length($1)); if (unit == "K") size=size/1024; else if (unit == "G") size=size*1024; print size,$2 }' | sort -rh -k1,1
